2ab9dd90f3375038aaf8e4fc332b32cad64d440f
nixezz <ban.rak228@gmail.com>
2025-08-12 11:28:23 +0300

    Creating an interface for commands

---
+++ src/main/java/commands/Command.java
@@ -0,0 +1,8 @@
+package commands;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public interface Command {
+    void execute(Update update) throws TelegramApiException;
+}
